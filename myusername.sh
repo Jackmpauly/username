32a428962a0a20a4ab9f857db97f254ef14d0e80
Jack Pauly <pauly@chapman.edu>
2019-11-08 10:49:16 -0800

    Adds username.sh

---
+++ myusername.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+# myusername.sh
+# author
+echo "Enter your username: "
+read USR
+while echo "$USR" | egrep -v "^[a-z][a-z0-9_]{2,11}$" > /dev/null 2>&1
+do
+	echo "You must enter a valid username - alphanumeric values only!"
+	echo "Enter your username:  "
+	read USR
+done
+echo "Thank you"
